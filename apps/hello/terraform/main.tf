69277bf1ed91bbad082d97f28a9c9f0c3b948d06
Robin Smorenburg <robin@robino.io>
2023-12-08 14:50:48 +0100

    Configure workload identity and add hello app

---
+++ apps/hello/terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = ">= 3.43"
+    }
+    random = {
+      version = ">= 3.4"
+    }
+    http = {
+      version = ">= 3.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
+  tenant_id       = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
+  use_msi         = true
+
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+# Create the resource group.
+resource "azurerm_resource_group" "default" {
+  name     = "rg-hello"
+  location = "northeurope"
+}
