fe9688bfd805970642664be5bd42ff785bd59b62
Robin Smorenburg <robin@robino.io>
2023-12-08 19:55:19 +0100

    Change client-id

--- apps/hello/terraform/main.tf
+++ apps/hello/terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 provider "azurerm" {
   subscription_id      = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
   tenant_id            = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
-  client_id            = "8612b1b8-e15f-4f31-9630-44c71715971e"
+  client_id            = "a28e49e2-df12-4271-b00b-529acb7a5401"
   use_oidc             = true
   oidc_token_file_path = "/var/run/secrets/azure/tokens/azure-identity-token"
 
