fe6a6a10f70c86baa6acadbceebaeb3d0e9c6acc
Robin Smorenburg <robin@robino.io>
2023-12-08 18:02:32 +0100

    Modify runner

--- apps/hello/terraform/main.tf
+++ apps/hello/terraform/main.tf
@@ -1,22 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      version = "3.43.0"
-    }
-    random = {
-      version = ">= 3.4"
-    }
-    http = {
-      version = ">= 3.2"
+      version = ">= 3.43"
     }
   }
 }
 
 provider "azurerm" {
-  subscription_id = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
-  tenant_id       = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
+#  subscription_id = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
+#  tenant_id       = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
   use_msi         = true
-  client_id       = "8612b1b8-e15f-4f31-9630-44c71715971e"
+#  client_id       = "8612b1b8-e15f-4f31-9630-44c71715971e"
 
   features {
     resource_group {
