a4698bc248f85e088285deb17c5641fd0d40fe57
Robin Smorenburg <robin@robino.io>
2023-12-08 18:42:22 +0100

    Specify IODC information

--- apps/hello/terraform/main.tf
+++ apps/hello/terraform/main.tf
@@ -7,10 +7,11 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
-  tenant_id       = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
-  use_msi         = true
-  client_id       = "8612b1b8-e15f-4f31-9630-44c71715971e"
+  subscription_id      = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24"
+  tenant_id            = "7ddc4c97-c5a0-4a29-ac83-59be0f280518"
+  client_id            = "8612b1b8-e15f-4f31-9630-44c71715971e"
+  use_oidc             = true
+  oidc_token_file_path = "/var/run/secrets/azure/tokens/azure-identity-token"
 
   features {
     resource_group {
