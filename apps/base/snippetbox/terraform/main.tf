cd97ab3bb3c971f71fc8542b89dc6d311b38e386
Robin Smorenburg <robin@robino.io>
2024-01-20 10:51:11 +0100

    Update MySQL dev_url in Terraform provider configuration
    The value of the "dev_url" parameter in the Atlas provider section for Terraform has been updated from a Docker link to a complete MySQL URL. This change is expected to improve database accessibility in the development environment by including more detailed connection information.

--- apps/base/snippetbox/terraform/main.tf
+++ apps/base/snippetbox/terraform/main.tf
@@ -28,7 +28,7 @@ provider "azurerm" {
 }
 
 provider "atlas" {
-  dev_url = "docker://mysql/8"
+  dev_url = "mysql://root:pass@mysql.default.svc.cluster.local:3306"
 }
 
 data "azurerm_client_config" "current" {}
