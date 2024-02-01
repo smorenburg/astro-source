ea6df344b80d4379072ec1b3080d912f58cf8c3a
Robin Smorenburg <robin@robino.io>
2024-02-01 23:27:51 +0100

    Relocate Import-Module Az.Accounts command in Create-ResourceGroup.ps1
    The Import-Module Az.Accounts command was initially at the top of the Create-ResourceGroup.ps1 script. However, it's been moved to a different location where its call is more contextually appropriate. This rearrangement aims to enhance the script's clarity and structure.

--- workflows/pwsh/scripts/Create-ResourceGroup.ps1
+++ workflows/pwsh/scripts/Create-ResourceGroup.ps1
@@ -8,15 +8,13 @@ param(
     [string]$ResourceGroupSuffix = "rg-"
 )
 
-Import-Module Az.Accounts
-
 $federatedToken = Get-Content $Env:AZURE_FEDERATED_TOKEN_FILE -Raw
 $random = -Join ("0123456789abcdef".tochararray() | Get-Random -Count 6 | ForEach-Object { [char]$_ })
 $resourceGroupName = $ResourceGroupSuffix + $random
 
+Import-Module Az.Accounts
 Connect-AzAccount -ApplicationId $Env:AZURE_CLIENT_ID -TenantId $Env:AZURE_TENANT_ID -SubscriptionId $SubscriptionId -FederatedToken $federatedToken
 
 Import-Module Az.Resources
-
 New-AzResourceGroup -Name $resourceGroupName `
     -Location $Location
