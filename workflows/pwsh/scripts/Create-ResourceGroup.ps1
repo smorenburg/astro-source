c2dd8bad7dd6e84e820c22cb4b618ef1b5e8aae4
Robin Smorenburg <robin@robino.io>
2024-02-01 23:26:03 +0100

    Refactor Create-ResourceGroup script parameters
    This commit refactors the Create-ResourceGroup PowerShell script. It introduces new parameters for SubscriptionId, Location, and ResourceGroupSuffix to make the script more flexible. Additionally, the way a random string is generated for ResourceGroupName has been modified for succinctness.

--- workflows/pwsh/scripts/Create-ResourceGroup.ps1
+++ workflows/pwsh/scripts/Create-ResourceGroup.ps1
@@ -1,14 +1,22 @@
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [string]$SubscriptionId = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24",
+    [Parameter()]
+    [string]$Location = "northeurope",
+    [Parameter()]
+    [string]$ResourceGroupSuffix = "rg-"
+)
+
 Import-Module Az.Accounts
 
-$azureFederatedTokenFileContent = Get-Content $Env:AZURE_FEDERATED_TOKEN_FILE -Raw
-$resourceGroupSuffix = "rg-tfstate-astro-neu-"
-$location = "northeurope"
-$random = (Get-Random -Minimum 0 -Maximum 99999).ToString('000000')
-$resourceGroupName = $resourceGroupSuffix + $random
+$federatedToken = Get-Content $Env:AZURE_FEDERATED_TOKEN_FILE -Raw
+$random = -Join ("0123456789abcdef".tochararray() | Get-Random -Count 6 | ForEach-Object { [char]$_ })
+$resourceGroupName = $ResourceGroupSuffix + $random
 
-Connect-AzAccount -ApplicationId $Env:AZURE_CLIENT_ID -TenantId $Env:AZURE_TENANT_ID -FederatedToken $azureFederatedTokenFileContent
+Connect-AzAccount -ApplicationId $Env:AZURE_CLIENT_ID -TenantId $Env:AZURE_TENANT_ID -SubscriptionId $SubscriptionId -FederatedToken $federatedToken
 
 Import-Module Az.Resources
 
 New-AzResourceGroup -Name $resourceGroupName `
-    -Location $location
+    -Location $Location
