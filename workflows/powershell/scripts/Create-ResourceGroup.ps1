ea1f86105b7ed721f882da235034979e0ca910da
Robin Smorenburg <robin@robino.io>
2024-02-03 14:48:34 +0100

    Remove optional Azure connection from Create-ResourceGroup.ps1
    The parameter that controlled the option to connect to Azure has been removed from the Create-ResourceGroup.ps1 script. Consequently, the script will always import the Connect-Azure module and establish a connection with Azure using the provided SubscriptionId. This change simplifies the flow by always ensuring a connection is established when creating a new resource group.

--- workflows/powershell/scripts/Create-ResourceGroup.ps1
+++ workflows/powershell/scripts/Create-ResourceGroup.ps1
@@ -1,5 +1,4 @@
 Param(
-    [bool]$ConnectAzure = $true,
     [string]$SubscriptionId,
     [string]$Location,
     [string]$ResourceGroupSuffix
@@ -8,12 +7,11 @@ Param(
 $random = -Join ("0123456789abcdef".tochararray() | Get-Random -Count 6 | ForEach-Object -Parallel { [char]$_ })
 $resourceGroupName = $ResourceGroupSuffix + $random
 
-If ($ConnectAzure)
-{
-    Import-Module $PSScriptRoot/modules/Connect-Azure/Connect-Azure.psm1
-    Connect-Azure -SubscriptionId $SubscriptionId
-}
+Import-Module $PSScriptRoot/modules/Connect-Azure/Connect-Azure.psm1
+
+Connect-Azure -SubscriptionId $SubscriptionId
 
 Import-Module Az.Resources
+
 New-AzResourceGroup -Name $resourceGroupName `
     -Location $Location
