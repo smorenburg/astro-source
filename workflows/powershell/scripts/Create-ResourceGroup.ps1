e6988b40d5fd79dc5dd52f0c1ea118839311d77d
Robin Smorenburg <robin@robino.io>
2024-02-03 16:16:14 +0100

    Refactor code for generating random strings
    The coding format for generating random strings in the 'Create-ResourceGroup.ps1' PowerShell script has been optimized for readability. The pipe-lined commands have been appropriately restructured for clarity and increased maintainability.

--- workflows/powershell/scripts/Create-ResourceGroup.ps1
+++ workflows/powershell/scripts/Create-ResourceGroup.ps1
@@ -4,7 +4,9 @@ Param(
     [string]$ResourceGroupSuffix
 )
 
-$random = -Join ("0123456789abcdef".tochararray() | Get-Random -Count 6 | ForEach-Object -Parallel { [char]$_ })
+$random = -Join ("0123456789abcdef".tochararray() |
+        Get-Random -Count 6 |
+        ForEach-Object -Parallel { [char]$_ })
 $resourceGroupName = $ResourceGroupSuffix + $random
 
 Import-Module -Name $PSScriptRoot/modules/Connect-Azure/Connect-Azure.psm1
