cc02023a865d7ccc854614ca677aaa8aa5cb7e09
Robin Smorenburg <robin@robino.io>
2024-02-03 13:29:53 +0100

    Improve readability and performance in Create-ResourceGroup script
    Modifications in this commit include breaking the Azure connection command into multiple lines for enhanced readability, and generating random strings in parallel for performance improvement. The default parameter values have been removed to simplify the code and give users greater flexibility.

--- workflows/powershell/scripts/Create-ResourceGroup.ps1
+++ workflows/powershell/scripts/Create-ResourceGroup.ps1
@@ -1,19 +1,18 @@
-[CmdletBinding()]
-param(
-    [Parameter()]
-    [string]$SubscriptionId = "ae9db8ac-2682-4a98-ad36-7d13b2bd5a24",
-    [Parameter()]
-    [string]$Location = "northeurope",
-    [Parameter()]
-    [string]$ResourceGroupSuffix = "rg-"
+Param(
+    [string]$SubscriptionId,
+    [string]$Location,
+    [string]$ResourceGroupSuffix
 )
 
 $federatedToken = Get-Content $Env:AZURE_FEDERATED_TOKEN_FILE -Raw
-$random = -Join ("0123456789abcdef".tochararray() | Get-Random -Count 6 | ForEach-Object { [char]$_ })
+$random = -Join ("0123456789abcdef".tochararray() | Get-Random -Count 6 | ForEach-Object -Parallel { [char]$_ })
 $resourceGroupName = $ResourceGroupSuffix + $random
 
 Import-Module Az.Accounts
-Connect-AzAccount -ApplicationId $Env:AZURE_CLIENT_ID -TenantId $Env:AZURE_TENANT_ID -SubscriptionId $SubscriptionId -FederatedToken $federatedToken
+Connect-AzAccount -ApplicationId $Env:AZURE_CLIENT_ID `
+    -TenantId $Env:AZURE_TENANT_ID `
+    -SubscriptionId $SubscriptionId `
+    -FederatedToken $federatedToken
 
 Import-Module Az.Resources
 New-AzResourceGroup -Name $resourceGroupName `
