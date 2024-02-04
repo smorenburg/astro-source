ee90cab0b3b90430675d5a98dc7b3a9d9b7c0087
Robin Smorenburg <robin@robino.io>
2024-02-04 15:23:55 +0100

    Refactor Connect-Azure module and add New-RandomString function
    The Connect-Azure module has been renamed to Tools and refactored to improve the error handling. The refactoring includes a change in how parameters for Connect-AzAccount are passed and how the federated token is read. In addition, the New-RandomString function has been added, expanding the module's functionality to provide a random string based on specified conditions.

---
+++ workflows/powershell/scripts/modules/Tools/Tools.psm1
@@ -0,0 +1,73 @@
+function Connect-Azure
+{
+    param(
+        [string]$SubscriptionId
+    )
+
+    Import-Module -Name Az.Accounts
+
+    try
+    {
+        $federatedToken = Get-Content -Path $Env:AZURE_FEDERATED_TOKEN_FILE -Raw
+    }
+    catch
+    {
+        Write-Output -InputObject $PSItem
+        exit 1
+    }
+
+    $account = @{
+        ApplicationId = $Env:AZURE_CLIENT_ID
+        TenantId = $Env:AZURE_TENANT_ID
+        SubscriptionId = $SubscriptionId
+        FederatedToken = $federatedToken
+    }
+
+    try
+    {
+        Connect-AzAccount @account -WarningAction:SilentlyContinue | Out-Null
+    }
+    catch
+    {
+        Write-Output -InputObject $PSItem
+        exit 1
+    }
+}
+
+Export-ModuleMember -Function Connect-Azure
+
+function New-RandomString
+{
+    param(
+        [int]$Characters,
+        [switch]$Lowercase,
+        [switch]$Uppercase,
+        [switch]$Numbers,
+        [switch]$Symbols
+    )
+
+    $input = $empty
+
+    if ($Lowercase)
+    {
+        $input += "abcdefghijklmnopqrstuwvxyz"
+    }
+    if ($Uppercase)
+    {
+        $input += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    if ($Numbers)
+    {
+        $input += "0123456789"
+    }
+    if ($Symbols)
+    {
+        $input += "~`! @#$%^&*()_-+={[}]|\:;`"'<,>.?/"
+    }
+
+    $string = -Join ($input.tochararray() | Get-Random -Count $Characters | ForEach-Object { [char]$PSItem })
+
+    return $string
+}
+
+Export-ModuleMember -Function New-RandomString
